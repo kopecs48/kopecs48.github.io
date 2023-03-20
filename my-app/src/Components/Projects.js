af62e769d3e373840919f8ba317cf3bacc645e31
Shane Kopec <shanekopec123@gmail.com>
2023-03-20 15:38:53 -0400

    mapping project data to html cards, basic layout

---
+++ my-app/src/Components/Projects.js
@@ -0,0 +1,104 @@
+import { projects } from '../data'
+import '../App.css';
+// import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function BasicExample() {
+  return (
+    <div className = "wrapper">
+      
+    {projects.map((project, index) => (
+     
+      <li className = "projects text-center" key = {project.id}>
+      <Card style={{ width: '40rem' , height: '40rem'}}>
+      <Card.Img variant="top" src = {project.image} style={{width: '40rem', height: '25rem'}}/>
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title>{project.name}</Card.Title>
+        <Card.Text>
+          {project.description}
+        </Card.Text>
+        
+      </Card.Body>
+      <Card.Footer style={{border: 'none'}}>
+        {project.link.length > 1 && (
+          <Button variant="primary" href={project.link}>See Project</Button>
+        )}
+      
+      </Card.Footer>
+      </Card>
+    </li>
+    
+    ))}
+    </div>
+  );
+}
+
+// export default BasicExample;
+
+
+
+
+
+
+
+
+
+
+
+// function BasicExample() {
+//   return (
+//     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+//       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+//         <div className="flex flex-col w-full mb-20">
+//           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+//           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+//             Apps I've Built
+//           </h1>
+//           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+//             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+//             facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+//             fuga dolore.
+//           </p>
+//         </div>
+//         <div className="flex flex-wrap -m-4">
+//           {projects.map((project) => (
+//             <a
+//               href="www.google.com"
+//               key={project.image}
+//               className="sm:w-1/2 w-100 p-4">
+//               <div className="flex relative">
+//                 <img
+//                   alt="gallery"
+//                   className="absolute inset-0 w-full h-full object-cover object-center"
+//                   src={project.image}
+//                   width = "480"
+//                   height= "270"
+//                 />
+//                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+//                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+//                     {project.description}
+//                   </h2>
+//                   <h1 className="title-font text-lg font-medium text-white mb-3">
+//                     {project.name}
+//                   </h1>
+//                   <p className="leading-relaxed">{project.description}</p>
+//                 </div>
+//               </div>
+//             </a>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+export default BasicExample;
+
+
+
+
+
+
---
+++ my-app/src/data.js
@@ -0,0 +1,65 @@
+import moodWebApp from './portfolio/moodWebApp.png'
+import metricLearn from './portfolio/metricLearnHome.png'
+import mitreHome from './portfolio/mitreHome.png'
+import musicBuddy from './portfolio/musicBuddyHome.png'
+import userLoginPage from './portfolio/userLoginHome.png'
+import plaHome from './portfolio/plaHome.png'
+const projects = [
+    {
+        name: "Mood Web App",
+        description: "The goal of this project was to create a RESTful web application with a /mood endpoint that allows logged in users" +
+                     "to post about how they feel and see all previous posts from that user. The users must add a title and body to each post," +
+                      "then the current date and time will be added as fields, along with a streak field which keeps track of how many days in" + 
+                      "a row this user has posted a mood status.",
+        image: moodWebApp,
+        link: 'skopec.pythonanywhere.com',
+        id: 1
+    },
+    {
+        name: "Django Music Player",
+        description: "The goal of this project was to create an all in one social media and music web application." + 
+                     "This was achieved by using the Django Framework and Spotifty python API. This web app allowed for" + 
+                     "music playback, playlist creation, and user interaction through profile and song comments.",
+        image: musicBuddy,
+        link: 'https://github.com/kopecs48/DjangoSpotify',
+        id : 2
+    },
+    {
+        name: "Metric Learn Data Visualization",
+        description: "Metric Learn is a libray from SKLearn that can be very powerful. Capable of creating metrics able to filter out the noise in data, inorder to help give more" +
+                      "accurate classifacations accomplished with machine learning methods",
+                     
+        image: metricLearn,
+        link: 'https://github.com/kopecs48/Metric-Learn-Data-Visualization',
+        id : 3
+    },
+    {
+        name: "User Login and Registration",
+        description: "An introduction to user registration and authentication accomplished with AWS Amplify and Cognito. Capable of displaying the current logged in user," +
+                      "initialize user into user pool after an automated verifaction link.",
+                     
+        image: userLoginPage,
+        link: 'https://github.com/kopecs48/user-registration-login',
+        id : 4
+    },
+    {
+        name: "Perceptron Learning Algorithm",
+        description: "The Perceptron is an algorithm for supervised learning of binary classifiers.A binary classifier is a function which can decide whether or not an input," +
+                      "represented by a vector of numbers, belongs to some specific class.",
+                     
+        image: plaHome,
+        link: 'https://github.com/kopecs48/PerceptronLearningAlgorithm',
+        id : 5
+    },
+    {
+        name: "MITRE ATT&CK Data Threat Analyzer",
+        description: "MITRE ATT&CK is a globally-accessible knowlege base of adversary tactices and techniques. Given data in the form of JSONs my goal was to parse the data, create an editable" +
+                      "table, and generate a modifiable pdf.",
+                     
+        image: mitreHome,
+        link: '#',
+        id : 6
+    },
+]
+
+export {projects}
